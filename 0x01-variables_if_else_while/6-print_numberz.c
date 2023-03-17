b8ee0d08e811acd07686359fc3f0a195fad49724
root <assem.akl2004@gmail.com>
2023-03-17 16:27:04 -0700

    first try

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		printf("%d\n", x++);
+	}
+	return (0);
+}
