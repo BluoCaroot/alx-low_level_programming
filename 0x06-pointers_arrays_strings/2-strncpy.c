468d68f6787ea1a23b6903f184bbb07efd005679
root <assem.akl2004@gmail.com>
2023-03-30 12:43:53 -0700

    f

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strncpy - copies a string to another
+ * @dest: input value
+ * @src: input value
+ * n: input value
+ *
+ * Return: dest
+ */
+char _strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int cnt;
+
+	cnt = 0;
+	while (src[cnt] != '\0' && cnt < n)
+	{
+		cnt++;
+	}
+	for ( i = 0 ; i < cnt ; ++i )
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+}
