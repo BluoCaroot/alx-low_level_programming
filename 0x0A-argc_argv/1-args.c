8e8aaf212fa7ff5ccf0d6df03adebeb7b7500d5c
root <assem.akl2004@gmail.com>
2023-04-08 06:42:31 -0700

    second

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints number of arguments passed into it
+ * @argc: argument that counts number of arguments
+ * @argv: argument that stores the strings in an array of char*
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%d", argc - 1)
+	return (0);
+}
