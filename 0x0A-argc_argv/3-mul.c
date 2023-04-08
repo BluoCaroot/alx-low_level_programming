983a0c74dd371b6303791880b05b3ac3dc8aafb7
root <assem.akl2004@gmail.com>
2023-04-08 06:58:54 -0700

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: number of ags
@@ -10,11 +11,17 @@ int main(int argc, char **argv)
 {
 	int x, y;
 
-	x = argv[1] - '0';
-	y = argv[2] - '0';
-	if (argc)
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	if (argc == 3)
 	{
 		printf("%d\n", x * y);
+		return (0);
 	}
-	return (0);
+	else
+	{
+		printf("error\n");
+		return (1);
+	}
+	
 }
