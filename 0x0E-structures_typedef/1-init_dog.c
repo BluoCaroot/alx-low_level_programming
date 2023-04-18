5b482c9ea69d397c24015f11b60eef70abc3c5fe
root <assem.akl2004@gmail.com>
2023-04-18 03:01:47 -0700

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
-#include "dog.h"
+#include <dog.h>
+
 /**
  * init_dog - initialize a variable of type struct dog
  * @d: struct name
@@ -7,13 +8,16 @@
  * @owner: pet owner
  * Return: void
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
+
+
 
