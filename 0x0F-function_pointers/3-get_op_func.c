690b8975a81bd2a67582559824602dcb539d5c14
root <assem.akl2004@gmail.com>
2023-04-21 16:45:47 -0700

    final

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * get_op_func - pointer to needed funcitons
+ * @s: operator
+ * Return: return of any of the others functions
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	 op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    	};
+    	int i = 0;
+
+	if (s)
+	{
+		while (*(ops[i].op) != *(s) && ops[i].op)
+			++i;
+		return (ops[i].f);
+	}
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+#include "function_pointers.h"
+/**
+ * main - main function
+ * @argc - number of arguments
+ * @argv - array of arguments
+ * Return: 0 if success
+ */
+
+int main(int argc, char **argv[])
+{
+	int x, y;
+	char *a;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	a = argv[2];
+
+	if (!get_op_func(a) || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit (99);
+	}
+	if (((*a) == '/' ) || ((*a) == '%') && y == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(a)(x, y);)
+}
