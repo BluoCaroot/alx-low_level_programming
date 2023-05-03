934ddaf0973a3594be52a71bee1fe0c81f15231d
root <assem.akl2004@gmail.com>
2023-05-03 14:49:38 -0700

    tenth

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * insert_nodeint_at_index - inserts node at index
+ * @head: pointer to head node
+ * @idx: indx to add data to
+ * @n: data
+ * Return: address of new code NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	listint_t *new = malloc(sizeof(listint_t));
+	listint_t *temp = *head;
+
+	if (!new || !head)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == x)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	
+	for (i = 0; temp && i < idx; ++i)
+	{
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return(new);
+		}
+		else
+			temp = temp->next;
+	}
+	return (NULL);
+}
