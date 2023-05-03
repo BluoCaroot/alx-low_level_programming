92668d6bc84e1d0311e89d97deb84d1c003d594b
root <assem.akl2004@gmail.com>
2023-05-03 13:30:38 -0700

    fix

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,11 +7,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	temp = head;
-	while (temp)
+	while (head)
 	{
+		temp = head->next;
 		free(head);
 		head = temp;
-		temp = temp->next;
 	}
 }
