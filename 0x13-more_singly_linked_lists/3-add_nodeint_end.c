360a71e0d567492b3378ebfd01312325bdeb7279
root <assem.akl2004@gmail.com>
2023-05-03 13:13:28 -0700

    fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,13 +7,13 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t new = malloc(sizeof(listint_t));
+	listint_t *new = malloc(sizeof(listint_t));
 
 	if (!new)
 		return (NULL);
-	while (h)
-		*h = (*h)->next;
-	*h = new;
+	while (*head)
+		*head = (*head)->next;
+	*head = new;
 	new->n = n;
 	new->next = NULL;
 	return (new);
