83d28ce3c140c5e095ef6f832deb172ff00a023f
root <assem.akl2004@gmail.com>
2023-05-03 14:21:41 -0700

    eighth

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns the node at the given index
+ * @head: pointer to head node
+ * @index: index to print
+ * Return: node or NULL
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t find = head;
+	unsigned int i = 0;
+
+	while (i++ < index && find)
+		find = find->next;
+	if (!find)
+		return (NULL);
+	return (find);
+}
