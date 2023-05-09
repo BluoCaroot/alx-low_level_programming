44f668e84cff2900a7ad01271baa713a827e87da
root <assem.akl2004@gmail.com>
2023-05-09 07:30:33 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,13 +13,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r;
 	char *buf;
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
 	r = read(fd, buf, letters);
+	if (r == -1)
+		return (0);
 	w = write(1, buf, r);
 
+	if (w == -1)
+		return (0);
 	free(buf);
 	close(fd);
 	return (w);
