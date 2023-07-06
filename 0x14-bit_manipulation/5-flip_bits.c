9c1c090da9f70fab3a37efa459879ef81ec9c0b3
BluoCaroot <assem.akl2004@gmail.com>
2023-07-06 11:34:24 -0700

    done

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,28 @@
+#include "main.h"
+unsigned int count_bits(unsigned long int n);
+/**
+ * flip_bits - counts number of different bits
+ * @n: first number
+ * @m: second number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits( unsigned long int n, unsigned long int m)
+{
+	return (count_bits(n ^ m));
+}
+/**
+ * count_bits - counts set bits
+ * @n: number to count
+ * Return: number of set bits
+ */
+unsigned int count_bits(unsigned long int n)
+{
+	unsigned int cnt = 0;
+
+	while (n)
+	{
+		cnt++;
+		n &= n - 1;
+	}
+	return (cnt);
+}
