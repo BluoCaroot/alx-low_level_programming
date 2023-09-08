f8450b24b02620d24a52b15841c137e3dc671daf
BluoCaroot <assem.akl2004@gmail.com>
2023-09-08 20:11:57 +0300

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
 {
 	hash_node_t *new = malloc(sizeof(hash_node_t));
-	
+
 	if (!new)
 		return (NULL);
 	new->key = strdup(key);
