b168daba6cb310e1f2acbe18674b3c1e7af2a621
BluoCaroot <assem.akl2004@gmail.com>
2023-09-08 18:52:13 +0300

    fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,15 +1,21 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - creates hash table
+ * @size: size of table
+ * Return: pointer to the table NULL if fail
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned int i;
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
-	
+
 	if (!hash)
 		return (NULL);
 
 	hash->size = size;
-	hash->array = malloc(sizeof(hash_node_t *) * size); 
+	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash->array)
 	{
 		free(hash);
